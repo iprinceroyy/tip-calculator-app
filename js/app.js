07be3ddf894b08039e8b32fa30f90951042f612c
Prince Roy <princeku.ray143@gmail.com>
2022-03-28 20:48:46 +0530

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,46 +1,33 @@
-const amount = document.getElementsByClassName("amt-input");
+const amount = document.getElementById("amount");
 const tipsBtn = document.getElementsByClassName("btn");
-const numOfPerson = document.getElementsByClassName("people");
+const numOfPersons = document.querySelector(".people");
 
-function getValues() {
+amount.addEventListener("change", getValues(amount));
 
-    let arr = [];
-    document.addEventListener('click', () => {
-        const val = document.activeElement.tagName;
-        const cls = document.getElementsByTagName(val)[0].className;
-        if (cls === "amt-input") {
-            const amount = document.getElementsByClassName("amt-input");
-            amount[0].addEventListener('change', () => {
-                console.log(amount[0].value);
-                arr[0] = amount[0].value;
-            });
-        } else if (cls === "btn") {
-            const tipsBtn = document.getElementsByClassName("btn");
-            tipsBtn[0].addEventListener('click', () => {
-                console.log(tipsBtn[0]);
-                arr[1] = tipsBtn[0];
-            });
-        } else {
-            const numOfPerson = document.getElementsByClassName("people");
-            numOfPerson[0].addEventListener('click', () => {
-                console.log(numOfPerson[0]);
-                arr[2] = numOfPerson[0];
-            })
-        }
-    });
+for (let i = 0; i < tipsBtn.length; i++) {
+    tipsBtn[i].addEventListener("click", handleChange);
+}
+numOfPersons.addEventListener("change", handleChange);
 
-    return arr;
+function handleChange(ele) {
+    return ele.value;
 }
 
+function getValues(element) {
+    const x = handleChange(element);
+    //console.log(x);
+    return x;
+}
 
+function calculate() {
+    const amt = amount.value;
+    const tip = tipsBtn.value;
+    const person = numOfPersons.value;
 
+    let total = 0;
+    total += (amt * (tip / 100)) / person;
 
-/*
-for (let i = 0; i < tipsBtn.length; i++) {
-    tipsBtn[i].addEventListener('click', () => {
-        console.log(tipsBtn[i].value);
-    })
-}*/
+    console.log(total);
+}
 
-let array = getValues();
-console.log(array);
+document.querySelector(".tip-amt").addEventListener("click", calculate);
