113cd3ca8ecaf261f1ba5d35d2330787bf5d8330
Prince Roy <princeku.ray143@gmail.com>
2022-05-04 16:21:29 +0530

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,11 @@
-const amount = document.getElementById("amount");
-const tipsBtn = document.getElementsByClassName("btn");
+const bill = document.getElementById("bill");
+const tip = document.getElementsByClassName("btn");
 const numOfPersons = document.querySelector(".people");
 
-amount.addEventListener("change", getValues(amount));
-
-for (let i = 0; i < tipsBtn.length; i++) {
-    tipsBtn[i].addEventListener("click", handleChange);
-}
-numOfPersons.addEventListener("change", handleChange);
-
-function handleChange(ele) {
-    return ele.value;
+const calcTip = (amount, tip, numOfpersons) => {
+    return (amount * tip / 100) / numOfpersons;
 }
 
-function getValues(element) {
-    const x = handleChange(element);
-    //console.log(x);
-    return x;
-}
 
-function calculate() {
-    const amt = amount.value;
-    const tip = tipsBtn.value;
-    const person = numOfPersons.value;
 
-    let total = 0;
-    total += (amt * (tip / 100)) / person;
-
-    console.log(total);
-}
-
-document.querySelector(".tip-amt").addEventListener("click", calculate);
+//console.log(calcTip(142.55, 15, 5));
